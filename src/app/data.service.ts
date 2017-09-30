6ece83a9c1dfdd4c6f8c8b427c6bff92465d426f
DRSOFTCUBA\jasiel <jasieltelles>
2017-09-30 17:43:26 -0300

    todo terminado

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,10 +7,15 @@ export class DataService {
   constructor(private http:Http) { }
 
 getData(){
+
 return this.http.get(" https://tienda-alimentos.firebaseio.com/.json")
 .map((response:Response)=>response.json());
 }
 
-
+SendData(data:any){
+const datos=JSON.stringify(data)
+return this.http.put(" https://tienda-alimentos.firebaseio.com/.json",datos)
+.map((response:Response)=>response.json());
+}
 
 }
